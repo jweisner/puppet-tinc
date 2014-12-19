7c335c9507cdbac681b385b64e0a77a665c1d9c9
Jesse Weisner <jesse@weisner.ca>
2014-12-18 19:55:03 -0800

    stub classes

---
+++ manifests/init.pp
@@ -0,0 +1,9 @@
+# URL: https://github.com/jweisner/puppet-tinc_wrapper.git
+# Author: Jesse Weisner <jesse@weisner.ca>
+# License: Apache 2.0
+class tinc(
+  $tinc_defaults = {},
+  $tinc_nets     = {},
+){
+  # do stuff
+}
---
+++ manifests/puppetmaster.pp
@@ -0,0 +1,8 @@
+# tinc::puppetmaster
+#
+# manage tinc key generation and access
+class tinc::puppetmaster (
+  $storage_path = "${::vardir}/tinc",
+){
+
+}

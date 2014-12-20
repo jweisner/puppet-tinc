3d7e14fca23059744549ccbc4b6ad87e68c103ed
Jesse Weisner <jesse@weisner.ca>
2014-12-19 16:15:34 -0800

    implement defaults and hiera merging

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,8 +2,51 @@
 # Author: Jesse Weisner <jesse@weisner.ca>
 # License: Apache 2.0
 class tinc(
-  $tinc_defaults = {},
-  $tinc_nets     = {},
+  $net_defaults       = {},
+  $net_defaults_merge = true,
+  $nets               = {},
+  $nets_merge         = true,
+  $package_list       = {'tinc' => {ensure => installed} },
+  $service_name       = 'tinc',
+  $service_enable     = true,
+  $service_ensure     = 'running',
 ){
-  # do stuff
+  create_resources('package', $package_list)
+  $package_array = keys($package_list)
+
+  file { 'etc/tinc/nets.boot':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0444',
+    content => '### WARNING: This file is controlled by Puppet ###',
+    require => Package[$package_array],
+  }->
+  service { $service_name:
+    ensure  => $service_ensure,
+    enable  => $service_enable,
+    require => Package[$package_array],
+  }
+
+  $net_defaults_override = $net_defaults_merge? {
+    false   => $net_defaults,
+    default => hiera_hash('tinc::net_defaults', $net_defaults)
+  }
+
+  $net_defaults_all = {
+    ensure         => present,
+    connectto      => [],
+    device         => '/dev/net/tun',
+    mode           => 'router',
+    net_enable     => true,
+    node_id        => $::hostname,
+    port           => '655',
+  }
+
+  $net_defaults_real = merge($net_defaults_all, $net_defaults_override)
+
+  $nets_real = $nets_merge? {
+    false   => $nets,
+    default => hiera_hash('tinc::nets', $nets),
+  }
 }

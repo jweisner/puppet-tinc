6d2adcaf5480b7858a2246ba397240fa9c774592
Jesse Weisner <jesse@weisner.ca>
2014-12-20 15:36:38 -0800

    create stub configs

--- manifests/net.pp
+++ manifests/net.pp
@@ -7,39 +7,35 @@ define tinc::net(
 
   #TODO: extract all of the necessary template vars
 
-  # file { "/etc/tinc/${net_id}":
-  #   ensure  => 'directory',
-  #   owner   => 'root',
-  #   group   => 'root',
-  #   mode    => '0750',
-  #   purge   => true,
-  #   recurse => true,
-  #   notify  => Service['tinc'],
-  # }->
-  # file { "/etc/tinc/${net_id}/tinc-up":
-  #   ensure  => 'file',
-  #   owner   => 'root',
-  #   group   => 'root',
-  #   mode    => '0640',
-  #   content => template('tinc/tinc-up.erb'),
-  #   notify  => Service['tinc'],
-  # }->
-  # file { "/etc/tinc/${net_id}/tinc-down":
-  #   ensure  => 'file',
-  #   owner   => 'root',
-  #   group   => 'root',
-  #   mode    => '0640',
-  #   content => template('tinc/tinc-down.erb'),
-  #   notify  => Service['tinc'],
-  # }->
-  # file { "/etc/tinc/${net_id}/tinc.conf":
-  #   ensure  => 'file',
-  #   owner   => 'root',
-  #   group   => 'root',
-  #   mode    => '0640',
-  #   content => template('tinc/tinc.conf.erb'),
-  #   notify  => Service['tinc'],
-  # }
+  file { "/etc/tinc/${net_id}":
+    ensure  => 'directory',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0750',
+    purge   => true,
+    recurse => true,
+  }->
+  file { "/etc/tinc/${net_id}/tinc-up":
+    ensure  => 'file',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0640',
+    content => template('tinc/tinc-up.erb'),,
+  }->
+  file { "/etc/tinc/${net_id}/tinc-down":
+    ensure  => 'file',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0640',
+    content => template('tinc/tinc-down.erb'),
+  }->
+  file { "/etc/tinc/${net_id}/tinc.conf":
+    ensure  => 'file',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0640',
+    content => template('tinc/tinc.conf.erb'),
+  }
 
   file { "/etc/tinc/${net_id}/hosts":
     ensure  => 'directory',
@@ -48,14 +44,13 @@ define tinc::net(
     mode    => '0750',
     purge   => true,
     recurse => true,
-    notify  => Service['tinc'],
   }
 
   $net_member_nodes   = $nets[$net_id]['member_nodes']
   $net_nodes_prefixed = prefix(keys($net_member_nodes), "${net_id}-")
-  notify { 'member_net_nodes':
-    message => inline_template("net_nodes_prefixed => <%= @net_nodes_prefixed.join(',') %>"),
-  }
+  # notify { 'member_net_nodes':
+  #   message => inline_template("net_nodes_prefixed => <%= @net_nodes_prefixed.join(',') %>"),
+  # }
   net_host{$net_nodes_prefixed:
     member_nodes => $net_member_nodes,
     net_id       => $net_id,

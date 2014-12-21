4294c0337d33b3de7cb5798fcc36350b0e2299a7
Jesse Weisner <jesse@weisner.ca>
2014-12-20 22:51:13 -0800

    notify service on changes

--- manifests/boot_net.pp
+++ manifests/boot_net.pp
@@ -1,10 +1,12 @@
-#
+### Warning: This file is controlled by Puppet ###
 define tinc::boot_net(
-  $net_id = $name,
+  $net_id       = $name,
+  $service_name = 'tinc',
 ){
   file_line { "tinc-boot-${net_id}":
     ensure => present,
     path   => '/etc/tinc/nets.boot',
     line   => $net_id,
+    notify => Service[$service_name],
   }
 }

ddf439cd31db53a68a7b31883aa9706ce2e970a1
Jesse Weisner <jesse@weisner.ca>
2014-12-20 20:08:51 -0800

    more informative "empty file" message

--- manifests/net_host.pp
+++ manifests/net_host.pp
@@ -10,7 +10,7 @@ define tinc::net_host(
   $node_id       = $member_nodes[$node_certname]['node_id']
   $external_ip   = $member_nodes[$node_certname]['external_ip']
   $internal_cidr = $member_nodes[$node_certname]['internal_cidr']
-  $public_key    = file("${key_source_path}/${net_id}/${node_certname}/rsa_key.pub", 'tinc/empty')
+  $public_key    = file("${key_source_path}/${net_id}/${node_certname}/rsa_key.pub", 'tinc/missing')
 
   file { "/etc/tinc/${net_id}/hosts/${node_id}":
     ensure  => 'file',

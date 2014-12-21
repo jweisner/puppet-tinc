48e6b67daa5cb20b2012575f284d0e0b7383ab8f
Jesse Weisner <jesse@weisner.ca>
2014-12-20 22:59:10 -0800

    start creating puppetmaster class

--- manifests/puppetmaster.pp
+++ manifests/puppetmaster.pp
@@ -1,8 +1,14 @@
 # tinc::puppetmaster
 #
 # manage tinc key generation and access
-class tinc::puppetmaster (
-  $storage_path = "${::vardir}/tinc",
-){
+# Author: Jesse Weisner <jesse@weisner.ca>
+# License: Apache 2.0
+class tinc::puppetmaster (){
+  $key_source_path = pick(hiera('tinc::key_source_path'), '/var/lib/puppet/tinc')
+  $nets            = pick(hiera_array('tinc::nets'), {})
+  $nets_list       = keys($nets)
 
+  puppetmaster_nets{$nets_list:
+    key_source_path => $key_source_path,
+  }
 }
